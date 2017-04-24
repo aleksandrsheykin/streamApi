b00a20a9f46c20a9f0b3f9d8b2f76c6a5d2b7402
Aleksandr <Lunatik_@bk.ru>
2017-04-24 20:45:59 +0300

    stream working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,12 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
     private static volatile SumVal sumVal = new SumVal();
     private static Finisher finisher;
@@ -8,7 +15,14 @@ public class Main {
         String resources[] = new String[]{"C:\\Users\\admin\\IdeaProjects\\streamApi\\1.txt", "2.txt", "3.txt"};
         //finisher = new Finisher(resources.length);
 
-        new Summator(resources[0], 0, sumVal, finisher).start();
+        Stream streamFromString = Arrays.stream("1 -23 2".split("\\s+"))
+                .filter((s)->Integer.valueOf(s) > 0)
+                .filter((s)->Integer.valueOf(s) %2 == 0);
+        //streamFromString.peek((e) -> System.out.print("," + e));
+        streamFromString.forEach(System.out::println);
+
+
+        //new Summator(resources[0], 0, sumVal, finisher).start();
         /*for (int i = 0; i < resources.length; i++) {
             new Summator(resources[i], i, sumVal, finisher).start();
         }*/
